7cff57556ae1945817c620ec182df910bf8c9388
Emanuel Ozorio Dias <emanoel.95@hotmail.com>
2024-04-30 00:46:40 -0300

    criação do metodo deeplink

---
+++ src/actionpay.ts
@@ -0,0 +1,60 @@
+import axios, { AxiosInstance } from 'axios';
+
+export class Actionpay {
+  private apiKey: string;
+  private api: AxiosInstance;
+
+  constructor(apiKey: string) {
+    this.apiKey = apiKey;
+
+    this.api = axios.create({
+      baseURL: 'https://actionpay.com.br/br-en/',
+    });
+  }
+
+  private async getEndpoint(endpoint: string, params: Record<string, any>): Promise<any> {
+    try {
+        const response = await this.api.get(endpoint, { params });
+        return response.data;
+    } catch (error: any) {
+        throw new Error(`Falha ao buscar dados do endpoint ${endpoint}: ${error.message}`);
+    }
+  }
+
+  /**
+   * Retorna o deeplink da URL enviada conforme as diretrizes do endpoint apiWmLinks
+   * 
+   * @param string url
+   * @param number offer
+   * @param number source
+   * 
+   * @return str
+   */
+  async deeplink(url: string, offer: number, source: number): Promise<any> {
+    const params = {
+        key: this.apiKey,
+        format: "xml",
+        offer: offer,
+        source: source
+    }
+
+    try {
+      const result = await this.getEndpoint("apiWmLinks", params);
+      var content = JSON.parse(result);
+
+      if (content.error) {
+        throw new Error(content.error);
+      } else {
+        for(var key2 in content.result.links){
+          if(/url\=/img.test(content.result.links[key2].url)) {
+              return content.result.links[key2].url.replace("example.com", encodeURIComponent(url));
+          }
+        }
+        
+        throw new Error("Esse programa não permite deeplink de uma url.");
+      }
+    } catch (error: any) {
+      throw new Error(`Falha ao buscar deeplink: ${error.message}`);
+    }
+  }
+}
